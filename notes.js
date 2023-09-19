490e2640d2990697321a360f4043960b0e2c7a7e
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-09-19 14:25:21 +0700

    Revert "create project"
    This reverts commit 8c7693bbd2ad6eb6bd06fb2b6b978ebd96d8f4b3.

--- notes.js
+++ notes.js
@@ -1,4 +1,4 @@
 // Văn Bình 14:14:49 19/08/2023
 
 //------------------------"branch template"-------------------
-// crate project with typescript: npx create-react-app client --template typescript
+// temaplte
