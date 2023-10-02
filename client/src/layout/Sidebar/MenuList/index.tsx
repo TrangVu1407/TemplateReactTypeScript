5aafdcc75b56fa2b1ad118f3edfc4f647e6e18d6
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-10-02 16:10:11 +0700

    tạm push code kiểm tra menu list lấy từ API, nếu không có quyền thì không không hiện màn hinh

--- client/src/layout/Sidebar/MenuList/index.tsx
+++ client/src/layout/Sidebar/MenuList/index.tsx
@@ -9,11 +9,63 @@ import MenuList1 from "../../../menu-items"
 
 const MenuList = () => {
   const theme = useTheme();
+  
+  let listMenuItem = [];
+  const permissions = JSON.parse(localStorage.getItem("permissions") || "{}");
+
+  for (let i = 0; i < MenuList1.items.length; i++) {
+    if (MenuList1.items[i].child === false) {
+      for (let j = 0; j < permissions.length; j++) {
+        if (MenuList1.items[i].path === permissions[j].screen) {
+          listMenuItem.push(MenuList1.items[i]);
+        }
+      }
+    } else if (MenuList1.items[i].child === true) {
+      let itemNav = [];
+      let itemGroup = [];
+
+      // navItem
+      let dataNav = MenuList1.items[i].itemNav ? MenuList1.items[i].itemNav : [];
+      if (dataNav != undefined) {
+        for (let k = 0; k < dataNav.length; k++) {
+          for (let j = 0; j < permissions.length; j++) {
+            if (permissions[j].screen === dataNav[k].path) {
+              itemNav.push(dataNav[k]);
+            }
+          }
+        }
+      }
+
+      // itemGroup
+      let dataGroup = MenuList1.items[i].itemGroup ? MenuList1.items[i].itemGroup : [];
+      if (dataGroup != undefined) {
+        for (let k = 0; k < dataGroup.length; k++) {
+          let itemNav = [];
+          let dataGroups = dataGroup[k].itemGropss ? dataGroup[k].itemGropss : [];
+          for (let m = 0; m < dataGroups.length; m++) {
+            for (let j = 0; j < permissions.length; j++) {
+              if (permissions[j].screen === dataGroup[k].itemGropss[m].path) {
+                itemNav.push(dataGroup[k].itemGropss[m]);
+              }
+            }
+          }
+          if (itemNav.length > 0) {
+            itemGroup.push({ name: dataGroup[k].name, icon: dataGroup[k].icon, itemGropss: itemNav })
+          }
+        }
+      }
+
+      // else
+      if (itemNav.length > 0 || itemGroup.length > 0) {
+        listMenuItem.push({ path: '', name: '', nameGroup: MenuList1.items[i].nameGroup, icon: MenuList1.items[i].icon, child: MenuList1.items[i].child, itemNav: itemNav, itemGroup: itemGroup });
+      }
+    }
+  }
 
   return (
     <Box sx={{ overflow: "auto" }}>
       <List>
-        {MenuList1.items.map((item, index) => (
+        {listMenuItem.map((item, index) => (
           !item.child ? (
             <NavLink to={item.path ? item.path : ''} key={item.path} className={theme.palette.mode === 'dark' ? 'link_dark' : 'link_light'}>
               <ListItem key={item.name} disablePadding>
