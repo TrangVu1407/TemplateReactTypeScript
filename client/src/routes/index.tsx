a831565d7357c0d87c4702101fbceb84c9c581bf
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-10-03 17:13:46 +0700

    check permissions? /home : login

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { useRoutes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useRoutes, useNavigate } from 'react-router-dom';
 import MainRoutes from './MainRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes'
 
@@ -8,17 +9,23 @@ import type { typeLocalStorage } from "../local-storage/localStorage"
 
 export default function ThemeRoutes() {
     const data: typeLocalStorage = JSON.parse(localStorage.getItem("localStorage") || "{}");
-    let children = [];
-    if (data.permissions) {
-        for (let i = 0; i < data.permissions.length; i++) {
-            for (let j = 0; j < MainRoutes.children.length; j++) {
-                if (data.permissions[i].screen === MainRoutes.children[j].path) {
-                    children.push(MainRoutes.children[j])
+    let navigate = useNavigate();
+    useEffect(() => {
+        if (!data.permissions) {
+            return navigate("/login");
+        }
+        let children = [];
+        if (data.permissions) {
+            for (let i = 0; i < data.permissions.length; i++) {
+                for (let j = 0; j < MainRoutes.children.length; j++) {
+                    if (data.permissions[i].screen === MainRoutes.children[j].path) {
+                        children.push(MainRoutes.children[j])
+                    }
                 }
             }
         }
-    }
-    MainRoutes.children = children;
+        MainRoutes.children = children;
+    }, [])
     axios.defaults.baseURL = config.API_URL;
 
     return useRoutes([MainRoutes, AuthenticationRoutes]);
