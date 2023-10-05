0a4467295495918919a0dc86e44ffbbcb6758464
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-10-05 09:43:13 +0700

    on-click to pages home

--- client/src/layout/Sidebar/InfoSidebar/index.tsx
+++ client/src/layout/Sidebar/InfoSidebar/index.tsx
@@ -1,10 +1,16 @@
 import { Toolbar, Typography } from '@mui/material';
 
 import config from "../../../config"
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+    let navigate = useNavigate();
+
+    const pageHome = () => {
+        navigate("/dashboard");
+    };
     return (
-        <Toolbar style={{ background: "orange" }}>
+        <Toolbar style={{ background: "orange", cursor: "pointer" }} onClick={pageHome}>
             <Typography
                 variant="h3"
                 noWrap
