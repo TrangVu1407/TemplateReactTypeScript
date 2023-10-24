559cb067da190b995bf30ae23cafca7c53aa6ef1
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-10-24 16:51:16 +0700

    from infodialog

--- client/src/views/pages/Product/ProductType/InfoDialog.tsx
+++ client/src/views/pages/Product/ProductType/InfoDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Stack, Button, Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions } from '@mui/material';
+import { Box, TextareaAutosize, TextField, Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material';
 
 interface Props {
     open: boolean;
@@ -7,25 +7,67 @@ interface Props {
 }
 
 const InfoDialog: React.FC<Props> = ({ open, closeOpen }) => {
+    const [valueTypeProduct, setValueTypeProduct] = React.useState("Quần áo");
+    const [valueDescribe, setValueDescribe] = React.useState("")
+    const [valueNotes, setValueNotes] = React.useState("")
+
+    const createTypeProduct = async () => {
+        console.warn("valueRef.current.value", `loại sản phẩm: ${valueTypeProduct} - mô tả: ${valueDescribe} - ghi chú: ${valueNotes}`);
+    }
 
     return (
         <Dialog
             open={open}
+            fullWidth={true}
             keepMounted
             aria-describedby="alert-dialog-slide-description"
         >
-            <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+            <DialogTitle sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'}}>{"$Thêm mới loại sản phẩm"}</DialogTitle>
             <hr />
             <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
-                </DialogContentText>
+                <Box component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1 },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        label="Tên loại sản phẩm"
+                        id="outlined-size-small"
+                        size="small"
+                        fullWidth
+                        value={valueTypeProduct}
+                        onChange={e => setValueTypeProduct(e.target.value)}
+                    />
+                    <TextField
+                        fullWidth
+                        multiline
+                        label="Mô tả"
+                        InputProps={{
+                            inputComponent: TextareaAutosize,
+                            rows: 3
+                        }}
+                        value={valueDescribe}
+                        onChange={e => setValueDescribe(e.target.value)}
+                    />
+                    <TextField
+                        fullWidth
+                        multiline
+                        label="Ghi chú"
+                        InputProps={{
+                            inputComponent: TextareaAutosize,
+                            rows: 3
+                        }}
+                        value={valueNotes}
+                        onChange={e => setValueNotes(e.target.value)}
+                    />
+                </Box>
             </DialogContent>
             <hr />
             <DialogActions>
                 <Button variant="outlined" onClick={() => closeOpen(!open)}>Đóng</Button>
-                <Button variant="contained">Thêm</Button>
+                <Button variant="contained" onClick={() => createTypeProduct()}>Thêm</Button>
             </DialogActions>
         </Dialog>
     )
