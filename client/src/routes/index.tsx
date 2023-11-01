6f834cd01098d20b27e12ec57ae5fa449fe8db78
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-11-01 11:20:57 +0700

    how to fix error connect API FE with BE

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -8,23 +8,33 @@ import config from '../config';
 import type { typeLocalStorage } from "local-storage/localStorage"
 
 export default function ThemeRoutes() {
-    const data: typeLocalStorage = JSON.parse(localStorage.getItem("localStorage") || "{}");
+    axios.defaults.baseURL = config.API_URL;
     let navigate = useNavigate();
     useEffect(() => {
+        const data: typeLocalStorage = JSON.parse(localStorage.getItem("localStorage") || "{}");
         if (!data.permissions) {
             return navigate("/login");
         }
         let children = [];
-            for (let i = 0; i < data.permissions.length; i++) {
-                for (let j = 0; j < MainRoutes.children.length; j++) {
-                    if (data.permissions[i].screen === MainRoutes.children[j].path) {
-                        children.push(MainRoutes.children[j])
-                    }
+        for (let i = 0; i < data.permissions.length; i++) {
+            for (let j = 0; j < MainRoutes.children.length; j++) {
+                if (data.permissions[i].screen === MainRoutes.children[j].path) {
+                    children.push(MainRoutes.children[j])
                 }
             }
+        }
         MainRoutes.children = children;
+
+        axios.interceptors.request.use(
+            (request) => {
+                const token = data.token;
+                if (token) {
+                    request.headers.Authorization = "Bearer " + token;
+                }
+                return request;
+            },
+        );
     }, [])
-    axios.defaults.baseURL = config.API_URL;
 
     return useRoutes([MainRoutes, AuthenticationRoutes]);
 }
