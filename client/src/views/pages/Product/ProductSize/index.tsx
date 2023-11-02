40f35aee6f1f5ca940032c7f8b2809f8f50317ad
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-11-02 14:37:23 +0700

    demo

--- client/src/views/pages/Product/ProductSize/index.tsx
+++ client/src/views/pages/Product/ProductSize/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
-const ProductType = () => {
+const initState = { count: 2 }
+const enum REDUCER_ACTION_TYPE { INCREMENT, DECREMENT }
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE
+}
+const reducer = (state: typeof initState, action: ReducerAction): typeof
+  initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 }
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
+
+type ChildrenType = {
+  children: (num: number) => React.ReactNode
+}
+
+const ProductSize = () => {
+  const [state, dispatch] = React.useReducer(reducer, initState);
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
   return (
-    <div>Loại sản phẩm</div>
+    <>
+      <div style={{ textAlign: "center", marginBottom: "10px" }}>
+        <h1>Kết quả: {state.count}</h1>
+
+
+        <Button variant="outlined" onClick={increment}>
+          Tăng
+        </Button>
+        <Button variant="outlined" color="error" onClick={decrement} style={{ marginLeft: "12px" }}>
+          Giảm
+        </Button>
+      </div>
+      <hr />
+    </>
   )
 }
 
-export default ProductType
+export default ProductSize
