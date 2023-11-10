f91353a0b2b2e50d2aeab7fcb43a720c02fae023
Huynh Van Binh <huynhvanbinh1606@gmail.com>
2023-11-10 09:26:46 +0700

    import it18next vào app.ts để thay đổi ngôn ngữ

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
+import React from 'react'
 import { CssBaseline, ThemeProvider, StyledEngineProvider } from '@mui/material';
 import { useSelector } from "react-redux";
 import Routes from 'routes';
 import themes from 'themes';
 import "./views/pages/pages.sass"
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: require('./local-storage/i18n/en/en.json')
+      },
+      vn: {
+        translation: require('./local-storage/i18n/vn/vn.json')
+      }
+    },
+    lng: 'vn', // default language
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
 export interface State {
   customization: {
     themes: boolean;
